489ff3d3f0bfdc0e523f7554221a770b6e15265b
DeveloperJose <josegperez@mail.com>
2020-04-30 00:25:47 -0600

    Add base project and appointments

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'appointments/appointments.dart';
+import 'contacts/avatar.dart';
+import 'contacts/contacts.dart';
+import 'notes/notes.dart';
+import 'tasks/tasks.dart';
+
+void main() {
+  startMeUp() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    Avatar.docsDir = await getApplicationDocumentsDirectory();
+    runApp(FlutterBook());
+  }
+
+  startMeUp();
+}
+
+class FlutterBook extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: true,
+        title: 'FlutterBook',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+                appBar: AppBar(
+                    title: Text('FlutterBook by Jose G. Perez'),
+                    bottom: TabBar(tabs: [
+                      Tab(icon: Icon(Icons.date_range), text: 'Appointments'),
+                      Tab(icon: Icon(Icons.contacts), text: 'Contacts'),
+                      Tab(icon: Icon(Icons.note), text: 'Notes'),
+                      Tab(icon: Icon(Icons.assignment_turned_in), text: 'Tasks'),
+                    ])),
+                body: TabBarView(children: [
+                  Appointments(),
+                  Contacts(),
+                  Notes(),
+                  Tasks(),
+                ]))));
+  }
+}
+
+class Dummy extends StatelessWidget {
+  final String _title;
+
+  Dummy(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(_title));
+  }
+}
